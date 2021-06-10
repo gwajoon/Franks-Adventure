6c1ba15d47dd35f994c54bff0e5fac5dec6e0344
jettyevy <siejiesiang@gmail.com>
2021-06-10 23:25:39 +0800

    portal fades, scene switching to indoors

--- Assets/Scripts/SceneManagement/Portal.cs
+++ Assets/Scripts/SceneManagement/Portal.cs
@@ -17,17 +17,27 @@ public class Portal : MonoBehaviour, IPlayerTriggerable
         this.player = player;
         StartCoroutine(SwitchScene());
     }
+    
+    Fader fader;
+    private void Start()
+    {
+      fader = FindObjectOfType<Fader>();  
+    }
+    
 
     IEnumerator SwitchScene()
     {
         DontDestroyOnLoad(gameObject);
 
         GameController.Instance.PauseGame(true);
+        yield return fader.FadeIn(0.5f);
 
         yield return SceneManager.LoadSceneAsync(sceneToLoad);
         
         var destPortal = FindObjectsOfType<Portal>().First(x => x != this && x.destinationPortal == this.destinationPortal);
         player.Character.SetPositionAndSnapToTile(destPortal.SpawnPoint.position);
+
+        yield return fader.FadeOut(0.5f);
 
         GameController.Instance.PauseGame(false);
 
