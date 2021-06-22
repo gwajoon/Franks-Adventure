f90ead0042b4602f0c235e307b917e5c16a98f57
GwaJoon <hengguojun25@gmail.com>
2021-06-22 23:11:14 +0800

    Linked all maps together
    - added cave map

--- Assets/Scripts/SceneManagement/Desert.cs
+++ Assets/Scripts/SceneManagement/Desert.cs
@@ -14,16 +14,24 @@ public class Desert : MonoBehaviour
     public List<GameObject> NPC;
     public List<GameObject> SNPC;
 
+    public bool isActivated;
+
     void Update() 
     {
         if (BH1.activated == true && BH2.activated == true && BH3.activated == true && BH4.activated == true && BH5.activated == true)
-        {      
+        {   
+            isActivated = true;
+        }
+
+        if (isActivated) 
+        {       
             CameraShake.Instance.shakeDuration = 2f;
             BH1.activated = false;
             BH2.activated = false;
             BH3.activated = false;
             BH4.activated = false;
             BH5.activated = false;
+            isActivated = false;
             StartCoroutine(DialogueManager.Instance.ShowDialogue(solvedDialogue));
 
             foreach (GameObject npc in NPC) {
